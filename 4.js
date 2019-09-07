47539eeca6aa59d3cf61b28bfffd704b5dd84b60
Pandu Cahyo Pangestu <pandusudo@gmail.com>
2019-09-07 20:53:56 +0700

    Update 4.js

--- 4.js
+++ 4.js
@@ -1,14 +1,18 @@
 function groupNumber(str){
-  var ret = []
-  var num = str.match(/\d+/g).join("");
-  for(var i = 0; i < num.length; i += 3){
-    ret.push(num.substr(i, 3))
+  if(str.length < 2){
+  	return str
+  }else{
+    var ret = []
+    var num = str.match(/\d+/g).join("");
+    for(var i = 0; i < num.length; i += 3){
+      ret.push(num.substr(i, 3))
+    }
+    if(ret[ret.length-1].length == 1){
+      ret[ret.length-1] = ret[ret.length-2].charAt(2) + ret[ret.length-1];
+      ret[ret.length-2] = ret[ret.length-2].charAt(0) + ret[ret.length-2].charAt(1);
+    }
+    return ret.join('-');
   }
-  if(ret[ret.length-1].length == 1){
-    ret[ret.length-1] = ret[ret.length-2].charAt(2) + ret[ret.length-1];
-    ret[ret.length-2] = ret[ret.length-2].charAt(0) + ret[ret.length-2].charAt(1);
-  }
-  return ret.join('-');
 }
 
-console.log(groupNumber("9931341 -1 133233 14-232"));
+console.log(groupNumber("993141 -1 1323 14-232"));
